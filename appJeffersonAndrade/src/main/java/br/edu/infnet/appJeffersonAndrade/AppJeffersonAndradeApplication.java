e46d3e8d290bc40bae247b5e56a66abf8a4f0c8b
Jefferson Andrade <jffrsncrls@gmail.com>
2024-08-08 20:05:30 -0300

    inicio

---
+++ appJeffersonAndrade/src/main/java/br/edu/infnet/appJeffersonAndrade/AppJeffersonAndradeApplication.java
@@ -0,0 +1,13 @@
+package br.edu.infnet.appJeffersonAndrade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppJeffersonAndradeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppJeffersonAndradeApplication.class, args);
+	}
+
+}
---
+++ appJeffersonAndrade/src/test/java/br/edu/infnet/appJeffersonAndrade/AppJeffersonAndradeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.infnet.appJeffersonAndrade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppJeffersonAndradeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
